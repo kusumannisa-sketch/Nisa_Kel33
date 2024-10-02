318bf151de8d3dc8463daa816e40cb322e9a45de
dyunyunn <dyunita839@gmail.com>
2024-10-02 15:32:19 +0700

    Tugas modul 4_KEL 33.py

--- Tugas modul 4_KEL 33.py
+++ Tugas modul 4_KEL 33.py
@@ -23,3 +23,27 @@ class Kasir:
         # Menambahkan item ke keranjang (array/list)
         self.keranjang.append({
             "nama": nama_barang,
+            print("Nomor barang tidak valid.\n")
+
+    def checkout(self):
+        if not self.keranjang:
+            print("Keranjang belanja kosong.\n")
+        else:
+            total_belanja = sum(item["total"] for item in self.keranjang)
+            print("===== STRUK PEMBAYARAN =====")
+            self.lihat_keranjang()
+            print(f"Total yang harus dibayar: Rp{total_belanja}")
+
+            # Menggunakan pengkondisian untuk memeriksa pembayaran
+            while True:
+                try:
+                    bayar = float(input("Masukkan jumlah uang yang dibayarkan: "))
+                    if bayar >= total_belanja:
+                        kembalian = bayar - total_belanja
+                        print(f"Pembayaran berhasil. Kembalian Anda: Rp{kembalian}\n")
+                        self.keranjang.clear()  # Mengosongkan keranjang setelah checkout
+                        break
+                    else:
+                        print("Uang tidak cukup! Silakan coba lagi.")
+                except ValueError:
+                    print("Input tidak valid. Silakan masukkan angka yang benar.\n")
