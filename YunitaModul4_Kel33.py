946208e002733037059afe1d49e85a0ef257bca9
aliyaumu <aliyaumukholisoh@gmail.com>
2024-10-02 13:54:57 +0700

    TugasModul4_Kel33.py

--- YunitaModul4_Kel33.py
+++ YunitaModul4_Kel33.py
@@ -23,3 +23,28 @@ class Kasir:
         # Menambahkan item ke keranjang (array/list)
         self.keranjang.append({
             "nama": nama_barang,
+            "harga": harga_barang,
+            "jumlah": jumlah_barang,
+            "total": total_harga
+        })
+        print(f"{nama_barang} berhasil ditambahkan ke keranjang.\n")
+
+    def lihat_keranjang(self):
+        if not self.keranjang:
+            print("Keranjang belanja kosong.\n")
+        else:
+            print("===== KERANJANG BELANJA =====")
+            for i, item in enumerate(self.keranjang, 1):
+                # Menggunakan pengulangan untuk menampilkan setiap item
+                print(f"{i}. {item['nama']} - {item['jumlah']} x Rp{item['harga']} = Rp{item['total']}")
+            print("==============================\n")
+
+    def hapus_barang(self):
+        self.lihat_keranjang()
+        if self.keranjang:
+            pilihan = int(input("Masukkan nomor barang yang ingin dihapus: ")) - 1
+            # Menggunakan pengkondisian untuk memeriksa validitas input
+            if 0 <= pilihan < len(self.keranjang):
+                barang_terhapus = self.keranjang.pop(pilihan)
+                print(f"{barang_terhapus['nama']} berhasil dihapus dari keranjang.\n")
+            else:
