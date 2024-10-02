ffa029be89bf09061e7328679978aa771c3a95b4
kusumannisa-sketch <rahmaku.rk@gmail.com>
2024-10-02 13:58:30 +0700

    Nisa_Kel33.py

---
+++ Nisa_Kel33.py
@@ -0,0 +1,25 @@
+class Kasir:
+    def _init_(self):
+        self.keranjang = []
+
+    def tampilkan_menu(self):
+        print("===== MENU KASIR =====")
+        print("1. Tambah Barang")
+        print("2. Lihat Keranjang")
+        print("3. Hapus Barang")
+        print("4. Checkout")
+        print("5. Keluar")
+        print("======================")
+
+    def tambah_barang(self):
+        # Menggunakan variabel dan tipe data
+        nama_barang = input("Masukkan nama barang: ")
+        harga_barang = float(input("Masukkan harga barang: "))
+        jumlah_barang = int(input("Masukkan jumlah barang: "))
+        
+        # Menghitung total harga
+        total_harga = harga_barang * jumlah_barang
+        
+        # Menambahkan item ke keranjang (array/list)
+        self.keranjang.append({
+            "nama": nama_barang,
